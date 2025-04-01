fa48825ccbfd61e6d2b52003c1cf04b9f0a58b05
3SSF <muhaddithumar@gmail.com>
2025-04-01 17:44:33 +0000

    added gravity 😎😎😎😎😎😎😎😎

--- main.rs
+++ main.rs
@@ -3,14 +3,18 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct Player;
 
+#[derive(Component)]
+struct Gravity(f32);
+
 const MOVEMENT_SPEED: f32 = 200.0;
+const GRAVITY: f32 = -50.;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::srgb(0.576, 0.671, 0.761)))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, init)
-        .add_systems(Update, handle_input)
+        .add_systems(Update, (handle_input, handle_gravity))
         .run();
 }
 
@@ -25,6 +29,7 @@ fn init(
         MeshMaterial2d(materials.add(Color::hsl(208., 0.75, 0.61))),
         Transform::from_xyz(0., -200., 0.),
         Player,
+        Gravity(GRAVITY),
     ));
 }
 
@@ -50,7 +55,17 @@ fn handle_input(
         if direction.length() > 0.0 {
             direction = direction.normalize();
         }
-        transform.translation += Vec3::new(direction.x, direction.y, 0.0) * MOVEMENT_SPEED * time.delta_seconds();
+        transform.translation += Vec3::new(direction.x, direction.y, 0.0) * MOVEMENT_SPEED * time.delta_secs();
+    }
+}
+
+fn handle_gravity( // don't mean too much to me!
+    time: Res<Time>,
+    mut sprite_position: Query<(&mut Transform, &mut Gravity), With<Player>>,
+){
+    for (mut transform, mut gravity) in &mut sprite_position {
+        gravity.0 -= 2.;
+        transform.translation += Vec3::new(0., 1., 0.) * gravity.0 * time.delta_secs();
     }
 }
 
