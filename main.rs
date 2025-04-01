2a57990e0b3870902dc13ee98f6eeb07cce16c81
3SSF <120362301+3SSF@users.noreply.github.com>
2025-04-01 17:07:37 +0000

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,56 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Player;
+
+const MOVEMENT_SPEED: f32 = 200.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::srgb(0.576, 0.671, 0.761)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, init)
+        .add_systems(Update, handle_input)
+        .run();
+}
+
+fn init(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+){
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(50., 50.))),
+        MeshMaterial2d(materials.add(Color::hsl(208., 0.75, 0.61))),
+        Transform::from_xyz(0., -200., 0.),
+        Player,
+    ));
+}
+
+fn handle_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut sprite_position: Query<&mut Transform, With<Player>>,
+){
+    for mut transform in &mut sprite_position {
+        let mut direction = Vec2::ZERO;
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction.x += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction.y -= 1.0;
+        }
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+        transform.translation += Vec3::new(direction.x, direction.y, 0.0) * MOVEMENT_SPEED * time.delta_seconds();
+    }
+}
+
